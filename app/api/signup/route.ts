87a2bbb2d6dc93f4a52f75fdacb1b5b9543a03c6
WAHJADAI <drafzaza123@icloud.com>
2024-03-14 08:42:25 +0700

    signup api

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,8 +1,31 @@
+import bcrypt from 'bcrypt'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 export async function POST(request: Request) {
-  const { email, password, name } = await request.json()
-  return Response.json({
-    email,
-    password,
-    name,
-  })
+  try {
+    const { email, password, name } = await request.json()
+    const hashedPassword = bcrypt.hashSync(password, 10)
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+      },
+    })
+    return Response.json({
+      message: 'create uer ok hafu.',
+      data: {
+        newUser,
+      },
+    })
+  } catch (error) {
+    return Response.json(
+      {
+        error,
+      },
+      { status: 500 }
+    )
+  }
 }
